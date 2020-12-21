1b48f4aeba8afcb13ae12e5aeff6e988d69b335c
Alex Biryukov <birukov20011@gmail.com>
2020-12-21 15:39:17 +0300

    page+start scripts

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+import { radioInit} from './radio.js';
+import { musicInit} from './music.js';
+import { videoInit} from './video.js';
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+
+const deactivation = () => {
+    temp.style.display = 'none'
+    playerBlock.forEach(item => item.classList.remove('active'));
+    playerBtn.forEach(item => item.classList.remove('active'));
+}
+
+playerBtn.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        deactivation();
+      btn.classList.add('active');  
+      playerBlock[i].classList.add('active')
+    })
+})
+
+
